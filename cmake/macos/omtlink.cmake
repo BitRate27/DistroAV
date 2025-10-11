cde13205d2a211eb06b21636a108aaa73f062cb3
BitRate27 <bgillocksbc@yahoo.com>
2025-10-11 15:03:40 -0400

    prevent multiple includes

--- cmake/macos/omtlink.cmake
+++ cmake/macos/omtlink.cmake
@@ -1,3 +1,4 @@
+include_guard(GLOBAL)
 set(omt_arch "MacOS")
 target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE ${CMAKE_SOURCE_DIR}/lib/omt/Libraries/${omt_arch}/libomt.dylib)
 target_include_directories(${CMAKE_PROJECT_NAME} PRIVATE ${CMAKE_SOURCE_DIR}/lib/omt/Libraries/${omt_arch})
--- cmake/windows/omtlink.cmake
+++ cmake/windows/omtlink.cmake
@@ -1,3 +1,4 @@
+include_guard(GLOBAL)
 set(omt_arch "Winx64")
 target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE ${CMAKE_SOURCE_DIR}/lib/omt/Libraries/${omt_arch}/libomt.lib)
 target_include_directories(${CMAKE_PROJECT_NAME} PRIVATE ${CMAKE_SOURCE_DIR}/lib/omt/Libraries/${omt_arch})
