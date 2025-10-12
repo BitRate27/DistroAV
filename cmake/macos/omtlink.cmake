893fd66ee213a452b58589c72e336e55b3a79531
BitRate27 <bgillocksbc@yahoo.com>
2025-10-12 16:29:47 -0400

    add omtlib.h to lib/omt

--- cmake/macos/omtlink.cmake
+++ cmake/macos/omtlink.cmake
@@ -1,4 +1,4 @@
 include_guard(GLOBAL)
 set(omt_arch "MacOS")
 target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE ${CMAKE_SOURCE_DIR}/lib/omt/Libraries/${omt_arch}/libomt.dylib)
-target_include_directories(${CMAKE_PROJECT_NAME} PRIVATE ${CMAKE_SOURCE_DIR}/lib/omt/Libraries/Winx64)
+target_include_directories(${CMAKE_PROJECT_NAME} PRIVATE ${CMAKE_SOURCE_DIR}/lib/omt)
